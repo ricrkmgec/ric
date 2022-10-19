10113b4669a43aaf02bcb63e38b0da3f818b6281
Elaf <skelaf2000@gmail.com>
2022-10-19 21:24:12 +0530

    Template Created

---
+++ src/components/Members/Core/Core.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Card from "../Cards/Card"
+
+const Core = () => {
+  return (
+    <div>
+      <h2>Faculty Advisor</h2>
+      <Card/>
+      <h2>Present Chairman</h2>
+      <Card/>
+      <h2>Core Members</h2>
+      <Card/>
+    </div>
+  )
+}
+
+export default Core
